c958b366f5cd18b2f63655e9e92a24514774bfe5
Feng Jiang <fjiang91@gmail.com>
2019-06-27 17:17:33 -0400

    feat: added firebase and connect firebase to Redux

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -63,8 +63,8 @@ const setSaltAndPassword = user => {
   }
 }
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
-User.beforeBulkCreate(users => {
-  users.forEach(setSaltAndPassword)
-})
+// User.beforeCreate(setSaltAndPassword)
+// User.beforeUpdate(setSaltAndPassword)
+// User.beforeBulkCreate(users => {
+//   users.forEach(setSaltAndPassword)
+// })
