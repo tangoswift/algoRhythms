e68c15fe99ea4be5fdf87523700862119aa50a93
Jorge Acosta <jh.acostadelapena@gmail.com>
2019-06-27 19:09:17 -0400

    Proof of concept. React only.

--- client/components/room/Room.js
+++ client/components/room/Room.js
@@ -2,31 +2,58 @@ import React from 'react'
 import AceEditor from 'react-ace'
 import WebWorker from '../../workers/WebWorker'
 import twoSumWorker from '../../workers/twoSumWorker'
+import db from '../../../server/firebase/fbConfig'
 
 class Room extends React.Component {
   constructor(props) {
     super(props)
+    this.room = db.collection('rooms')
     this.state = {
       worker: twoSumWorker,
       code:
-        '//Write a function to sum two numbers\nfunction twoSum (a,b){\n \n}'
+        '//Write a function to sum two numbers\nfunction twoSum (a,b){\n \n}',
+      result: ''
     }
     this.handleOnRun = this.handleOnRun.bind(this)
     this.onChange = this.onChange.bind(this)
   }
 
-  handleOnRun() {
-    this.worker = new WebWorker(this.state.worker)
-    this.worker.postMessage({code: this.state.code})
-    this.worker.addEventListener('message', e => {
-      console.log(e.data)
+  onChange = async newValue => {
+    await this.room.doc('1').set({
+      code: newValue
     })
-    setTimeout(() => this.worker.terminate(), 10000)
+    this.setState({...this.state, code: newValue})
   }
 
-  onChange(newValue) {
-    this.setState({code: newValue})
+  componentDidMount = async () => {
+    await this.room.doc('1').onSnapshot(doc => {
+      const data = doc.data().code || this.state.code
+      const result = doc.data().result
+      this.setState({...this.state, code: data, result: result})
+    })
   }
+
+  handleOnRun = () => {
+    this.worker = new WebWorker(this.state.worker)
+    this.worker.addEventListener('message', async e => {
+      await this.room.doc('1').set({
+        result: e.data + ''
+      })
+      this.setState({...this.state, result: e.data})
+    })
+
+    this.worker.postMessage({code: this.state.code})
+
+    //Terminate worker after 10s
+    setTimeout(() => this.worker.terminate(), 10000)
+    // this.setState({
+    //   worker: twoSumWorker,
+    //   code:
+    //     '//Write a function to sum two numbers\nfunction twoSum (a,b){\n \n}',
+    //   result: ''
+    // })
+  }
+
   render() {
     return (
       <div>
@@ -53,6 +80,7 @@ class Room extends React.Component {
         <button type="submit" name="action" onClick={this.handleOnRun}>
           RUN
         </button>
+        <text>{this.state.result}</text>
       </div>
     )
   }
--- client/store/index.js
+++ client/store/index.js
@@ -4,28 +4,27 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 //Initialize firestore/firebase with Redux/Firebase
-import {reduxFirestore, getFirestore, firestoreReducer} from 'redux-firestore'
-import {
-  reactReduxFirebase,
-  getFirebase,
-  firebaseReducer
-} from 'react-redux-firebase'
+// import {reduxFirestore, getFirestore, firestoreReducer} from 'redux-firestore'
+// import {
+//   reactReduxFirebase,
+//   getFirebase,
+//   firebaseReducer
+// } from 'react-redux-firebase'
 import fbConfig from '../../server/firebase/fbConfig'
 
 const reducer = combineReducers({
-  user,
-  firestore: firestoreReducer,
-  firebase: firebaseReducer
+  user
+  // firestore: firestoreReducer,
+  // firebase: firebaseReducer
 })
 const middleware = composeWithDevTools(
   //Add extra arguments to the Redux Store with access firestore/firebase
-  applyMiddleware(
-    thunkMiddleware.withExtraArgument({getFirestore, getFirebase}),
-    createLogger({collapsed: true})
-  ),
+  applyMiddleware()
+  // thunkMiddleware.withExtraArgument({getFirestore, getFirebase}),
+  // createLogger({collapsed: true})
   //Connect firebase with Redux Store
-  reduxFirestore(fbConfig),
-  reactReduxFirebase(fbConfig)
+  // reduxFirestore(fbConfig),
+  // reactReduxFirebase(fbConfig)
 )
 const store = createStore(reducer, middleware)
 
