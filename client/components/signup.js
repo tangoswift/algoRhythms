5a5710a0c95cdb166dee039d32d28ff7673c83be
Jason Cho <jason.cho.86@gmail.com>
2019-06-30 01:47:18 -0400

    feat: added css styling for sign-up page

--- client/components/signup.js
+++ client/components/signup.js
@@ -3,6 +3,52 @@ import history from '../history'
 import {connect} from 'react-redux'
 import {signUpThunk} from '../store/auth'
 
+// Material UI Dependencies
+import {withStyles, createMuiTheme} from '@material-ui/core/styles'
+import {ThemeProvider} from '@material-ui/styles'
+import PropTypes from 'prop-types'
+import TextField from '@material-ui/core/TextField'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import {blue} from '@material-ui/core/colors'
+
+/**
+ * MATERIAL UI
+ */
+const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3)
+  },
+  TextField: {
+    padding: '0 0px',
+    margin: '0 0px',
+    verticalAlign: 'middle'
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    palette: {
+      primary: blue
+    }
+  }
+})
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#01579b'
+    }
+  }
+})
+
 /**
  * COMPONENT
  */
@@ -28,36 +74,89 @@ class SignUp extends React.Component {
   }
 
   render() {
+    const {classes} = this.props
     const {authError} = this.props
     return (
-      <div>
-        <form onSubmit={this.handleOnSubmit}>
-          <input
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleOnChange}
-          />
-          <input
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleOnChange}
-          />
-          <input
-            name="email"
-            value={this.state.email}
-            onChange={this.handleOnChange}
-          />
-          <input
-            name="password"
-            value={this.state.password}
-            onChange={this.handleOnChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        {authError}
-      </div>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <form onSubmit={this.handleOnSubmit} className={classes.form}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className={classes.textField}
+                    required
+                    fullWidth
+                    id="firstName"
+                    name="firstName"
+                    label="First Name"
+                    value={this.state.firstName}
+                    onChange={this.handleOnChange}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className={classes.textField}
+                    required
+                    fullWidth
+                    id="lastName"
+                    name="lastName"
+                    label="Last Name"
+                    value={this.state.lastName}
+                    onChange={this.handleOnChange}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    className={classes.textField}
+                    required
+                    fullWidth
+                    id="email"
+                    name="email"
+                    label="Email"
+                    value={this.state.email}
+                    onChange={this.handleOnChange}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="password"
+                    name="password"
+                    label="Password"
+                    value={this.state.password}
+                    onChange={this.handleOnChange}
+                    variant="outlined"
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+            </form>
+            {authError}
+          </div>
+        </Container>
+      </ThemeProvider>
     )
   }
+}
+
+SignUp.propTypes = {
+  classes: PropTypes.object.isRequired
 }
 
 const mapState = state => {
@@ -72,4 +171,4 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(SignUp)
+export default withStyles(styles)(connect(mapState, mapDispatch)(SignUp))
