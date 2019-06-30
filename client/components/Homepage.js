89965c614207a4e9382adb842effd653f0119df0
Sidharth Nambiar <snambiar01@gmail.com>
2019-06-30 13:52:05 -0400

    routed to the homepage

---
+++ client/components/Homepage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+/**
+ * COMPONENT
+ */
+export const Homepage = props => {
+  return (
+    <div>
+      <h3>Get in the rhythm of solving code in pairs</h3>
+      <div>
+        <img
+          src="https://camo.githubusercontent.com/e5e84ef5a06e6435b70c8a4fc2a3324169dfd4c1/68747470733a2f2f7777772e657665726e6f74652e636f6d2f6c2f41415556584346745a5f5648356f41516e37334a576d77665249564d5a395272655877422f696d6167652e706e67"
+          width="1000"
+          height="600"
+          display="flex"
+          align="right"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
