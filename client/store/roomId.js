24c7794f6a47d28089555d6da1f9945dab59ff07
Feng Jiang <fjiang91@gmail.com>
2019-07-01 15:28:21 -0400

    feat: added shared code feature

--- client/store/roomId.js
+++ client/store/roomId.js
@@ -3,6 +3,7 @@
  */
 const ADD_ROOM = 'ADD_ROOM'
 const GET_ROOMS = 'GET_ROOMS'
+const CHANGE_CODE = 'CHANGE_CODE'
 
 /**
  * INITIAL STATE
@@ -14,6 +15,7 @@ const defaultRoomId = ''
  */
 const addRoom = roomId => ({type: ADD_ROOM, roomId})
 const getRooms = rooms => ({type: GET_ROOMS, rooms})
+const changeCode = () => ({type: CHANGE_CODE})
 
 /**
  * THUNK CREATORS
@@ -25,27 +27,35 @@ export const addRoomThunk = roomInfo => async (
 ) => {
   try {
     const firestore = getFirestore()
-    const res = await firestore.collection('rooms').add(roomInfo)
-
+    const res = await firestore.collection('rooms').add({
+      name: roomInfo.name,
+      instructions: roomInfo.instructions,
+      code: ' '
+    })
     dispatch(addRoom(res.id))
   } catch (err) {
     console.log(err)
   }
 }
 
-// export const getRoomsThunk = () => async (
-//   dispatch,
-//   getState,
-//   {getFirestore}
-// ) => {
-//   try {
-//     const firestore = getFirestore()
-//     const res = await firestore.collection('rooms').get()
-//     dispatch(getRooms(res.firestore.data.rooms))
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
+export const changeCodeThunk = (roomId, code) => async (
+  dispatch,
+  getState,
+  {getFirestore}
+) => {
+  try {
+    const firestore = getFirestore()
+    const res = await firestore
+      .collection('rooms')
+      .doc(roomId)
+      .update({
+        code: code
+      })
+    dispatch(changeCode())
+  } catch (error) {
+    console.error('TCL: changeCodeThunk -> error', error)
+  }
+}
 
 /**
  * REDUCER
@@ -53,6 +63,8 @@ export const addRoomThunk = roomInfo => async (
 export default function(state = defaultRoomId, action) {
   switch (action.type) {
     case ADD_ROOM:
+      return action.roomId
+    case CHANGE_CODE:
       return action.roomId
     case GET_ROOMS:
       return action.rooms
