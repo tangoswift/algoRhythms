821373f1e3def2384be131cd7769f8d22c12d91a
Feng Jiang <fjiang91@gmail.com>
2019-07-02 11:32:52 -0400

    feat: styled navbar

--- client/components/ProblemsSolved.js
+++ client/components/ProblemsSolved.js
@@ -1,44 +1,12 @@
 /* eslint-disable no-script-url */
 
-import React, {Component} from 'react'
-import Link from '@material-ui/core/Link'
+import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
-import {getRoomHistoryThunk} from '../store/user'
-import {connect} from 'react-redux'
-
-// import Title from './Title';
-
-// Generate Order Data
-// function createData(id, date, partner, problemType, status, pointsEarned) {
-//   return {id, date, partner, problemType, status, pointsEarned}
-// }
-
-// const rows = [
-//   createData(
-//     0,
-//     '16 Mar, 2019',
-//     'Elvis Presley',
-//     'Binary Search Tree',
-//     'Yes',
-//     2
-//   ),
-//   createData(1, '16 Mar, 2019', 'Paul McCartney', 'Find Min/Max', 'No', 0),
-//   createData(2, '16 Mar, 2019', 'Tom Scholz', 'Dynamic Programming', 'Yes', 1),
-//   createData(
-//     3,
-//     '16 Mar, 2019',
-//     'Michael Jackson',
-//     'Array and Objects',
-//     'No',
-//     0
-//   ),
-//   createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Hash Table', 'Yes', 5)
-// ]
 
 const useStyles = makeStyles(theme => ({
   seeMore: {
