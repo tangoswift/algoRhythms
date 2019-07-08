0c9a8b3f6c46803c7fb2374009b677bbdb95f1a8
Jason Cho <jason.cho.86@gmail.com>
2019-07-08 00:51:32 -0400

    feat: added Intesection to list of algo problems

---
+++ client/workers/Intersection.js
@@ -0,0 +1,41 @@
+export default () => {
+  self.addEventListener('message', e => {
+    if (!e) return undefined
+    try {
+      const inputs = [
+        [[1, 4, 9, 10, 11], [2, 3, 4, 5, 8, 10]],
+        [[5, 4, 1, 7, 2], [4, 2, 3, 5]]
+      ]
+      const outputs = [[4, 10], [5, 4, 2]]
+
+      let code = e.data
+      let bodyCode = code.slice(code.indexOf('{') + 1)
+      bodyCode = bodyCode.slice(0, -1)
+      let intersection = new Function('arrA', 'arrB', bodyCode + '')
+
+      const results = []
+      inputs.forEach((input, idx) => {
+        let intersectionArr = intersection(input[0], input[1])
+        let isCorrect = true
+        if (intersectionArr.length !== outputs[idx].length) {
+          isCorrect = false
+        } else {
+          for (let i = 0; i < outputs[idx].length; i++) {
+            if (intersectionArr[i] !== outputs[idx][i]) {
+              isCorrect = false
+              break
+            }
+          }
+        }
+        let res = `Expected [${input[0]}] and [${input[1]}] to return [${
+          outputs[idx]
+        }]: ${isCorrect}`
+        results[idx] = res
+      })
+
+      postMessage(results)
+    } catch (error) {
+      postMessage([error.message])
+    }
+  })
+}
