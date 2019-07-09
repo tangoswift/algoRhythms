bbe48b8ded4dba5af1a69ed1a896a3a8b97da466
Feng Jiang <fjiang91@gmail.com>
2019-07-09 13:20:41 -0400

    feat: refactored room component to query single room

--- client/components/room.js
+++ client/components/room.js
@@ -6,6 +6,7 @@ import intersection from '../workers/Intersection'
 import returnNegative from '../workers/ReturnNegative'
 import palindromeWorker from '../workers/PalindromeWorker'
 import reverseWordsWorker from '../workers/ReverseWordsWorker'
+import Loading from './Loading'
 import RoomResults from './RoomResults'
 import {compose} from 'redux'
 import {firestoreConnect} from 'react-redux-firebase'
@@ -76,99 +77,107 @@ class Room extends React.Component {
   }
 
   render() {
-    let id = this.props.match.params.id
-    let name = ''
-    let results = []
-    let code = ''
-    let instructions = ''
-    let start = false
-    let solved = false
-    let visible = true
-    const classes = this.props
-    const {profile} = this.props
+    let {room} = this.props
 
-    if (this.props.rooms && this.props.rooms[id]) {
-      name = this.props.rooms[id].name
-      code = this.props.rooms[id].code
-      instructions = this.props.rooms[id].instructions
-      results = this.props.rooms[id].result
-      start = this.props.rooms[id].start
-      solved = this.props.rooms[id].solved
-      visible = this.props.rooms[id].visible
-    }
+    /**
+     * Check if room exist
+     */
+    if (!room) {
+      return <Loading />
+    } else {
+      let id = this.props.match.params.id
+      const classes = this.props
+      const {profile} = this.props
+      let {
+        name,
+        result,
+        code,
+        instructions,
+        start,
+        solved,
+        visible
+      } = this.props.room
 
-    return (
-      <Container className={classes.root}>
-        <Box bgcolor="text.hint" color="background.paper">
-          {start && (
-            <Typography align="center">
-              You are currently the {profile.role}
-            </Typography>
-          )}
-          <Typography align="center" component="div" variant="body1">
-            Get Into The Rhythm: {id}
-            {!solved && (
-              <Countdown
-                start={start}
-                roomId={id}
-                role={profile.role}
-                visible={visible}
-              />
+      return (
+        <Container className={classes.root}>
+          <Box bgcolor="text.hint" color="background.paper">
+            {start && (
+              <Typography align="center">
+                You are currently the {profile.role}
+              </Typography>
             )}
-          </Typography>
-        </Box>
-        <Grid container spacing={2} className="room">
-          <Grid item xs={8} pr={0}>
-            <Typography variant="h6">Instructions: {instructions}</Typography>
-            <AceCodeEditor
-              code={code}
-              onChange={this.onChange}
-              role={profile.role}
-              start={start}
-            />
-            <Grid container justify="space-between">
-              <Button
-                type="submit"
-                name="action"
-                onClick={e => this.handleOnRun(e, code, name)}
-                variant="contained"
-                color="primary"
+            <Typography align="center" component="div" variant="body1">
+              Get Into The Rhythm: {id}
+              {!solved && (
+                <Countdown
+                  start={start}
+                  roomId={id}
+                  role={profile.role}
+                  visible={visible}
+                />
+              )}
+            </Typography>
+          </Box>
+          <Grid container spacing={2} className="room">
+            <Grid item xs={8} pr={0}>
+              <Typography variant="h6">Instructions: {instructions}</Typography>
+              <AceCodeEditor
+                code={code}
+                onChange={this.onChange}
+                role={profile.role}
+                start={start}
+              />
+              <Grid container justify="space-between">
+                <Button
+                  type="submit"
+                  name="action"
+                  onClick={e => this.handleOnRun(e, code, name)}
+                  variant="contained"
+                  color="primary"
+                >
+                  RUN
+                </Button>
+                <Button
+                  type="button"
+                  onClick={this.redirectToTarget}
+                  variant="contained"
+                  color="primary"
+                >
+                  BAIL
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <Box
+                bgcolor="text.primary"
+                color="background.paper"
+                height="100%"
               >
-                RUN
-              </Button>
-              <Button
-                type="button"
-                onClick={this.redirectToTarget}
-                variant="contained"
-                color="primary"
-              >
-                BAIL
-              </Button>
+                <iframe
+                  src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=c5b89831-6e30-4f25-a41e-d19d8b84ae1f&room=${id}&iframe=true`}
+                  width="100%"
+                  height="50%"
+                  allow="microphone; camera"
+                />
+
+                <Typography variant="h6">Test Results: </Typography>
+                <RoomResults result={result} />
+              </Box>
             </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Box bgcolor="text.primary" color="background.paper" height="100%">
-              <iframe
-                src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=c5b89831-6e30-4f25-a41e-d19d8b84ae1f&room=${id}&iframe=true`}
-                width="100%"
-                height="50%"
-                allow="microphone; camera"
-              />
-
-              <Typography variant="h6">Test Results: </Typography>
-              <RoomResults results={results} />
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    )
+        </Container>
+      )
+    }
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
+  const roomId = ownProps.match.params.id
+  const rooms = state.firestore.data.rooms
+  const room = rooms ? rooms[roomId] : null
   return {
     profile: state.firebase.profile,
-    rooms: state.firestore.data.rooms,
+    room: room,
     users: state.firestore.data.users
   }
 }
